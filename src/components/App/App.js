e75f10f6aec137fd065f45ebf39d24b1a482ba91
Rafael Cardoso <usacb@hotmail.com>
2022-06-09 12:41:52 -0500

    Second commit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,29 @@
 import './App.css';
 import Pairs from '../Pairs/Pairs';
+import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
 
-  const handleChange = (event) => {
-    console.log(event.target.select);
+  const [pair, setPair] = useState({});
+
+  const handleChange = async (event) => {
+    console.log(event.target.value);
+    const URL = "https://api.apilayer.com/currency_data/live?source=usd&currencies=EUR";
+    try{
+      //const response = await fetch(baseURL+apikey+query+movieTitle);
+      //const data = await response.json();
+      const response = await axios.get(URL, {
+        headers: {
+          apikey: "MISdtjtqxEPsuhsBPFhFMb8DHOmq1H32"
+        }
+      })
+      console.log(response.data);
+      setPair(response.data);
+    }catch (e) {
+      console.log(e);
+    }
+
   }
 
   return (
